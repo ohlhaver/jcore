9313bf3b4987bcc1627f16cde7bb6a0530e68de1
Ram Singla <ram.singla@gmail.com>
2010-03-10 15:44:59 +0530

    JCore Author Name removes email addresses

--- lib/clean.rb
+++ lib/clean.rb
@@ -20,6 +20,25 @@ module URI
   
 end
 
+module RFC822
+  EmailRegex = begin
+    qtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]'
+    dtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]'
+    atom = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-' +
+      '\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+'
+    quoted_pair = '\\x5c[\\x00-\\x7f]'
+    domain_literal = "\\x5b(?:#{dtext}|#{quoted_pair})*\\x5d"
+    quoted_string = "\\x22(?:#{qtext}|#{quoted_pair})*\\x22"
+    domain_ref = atom
+    sub_domain = "(?:#{domain_ref}|#{domain_literal})"
+    word = "(?:#{atom}|#{quoted_string})"
+    domain = "#{sub_domain}(?:\\x2e#{sub_domain})*"
+    local_part = "#{word}(?:\\x2e#{word})*"
+    addr_spec = "#{local_part}\\x40#{domain}"
+    pattern = /#{addr_spec}/
+  end
+end
+
 module JCore
   
   module Clean
@@ -126,6 +145,7 @@ module JCore
       
       def author( text, language = 'en' )
         text = pre_process( text, :strip_tags => true ).chars
+        text.gsub!( RFC822::EmailRegex, '') # removing emails
         text.gsub!(/\(|\)|\]|\[|\}|\}/, ', ')  # removing parenthesis
         text.gsub!(/\s+/m, ' ')                # remove white space
         text.strip!                            # remove trailing spaces
